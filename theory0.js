359daa39fbdc621f220a465005208679de25df8f
gary <gary@spoonradio.co>
2021-11-03 16:50:22 +0900

    Edit theory 0

--- theory0.js
+++ theory0.js
@@ -1,23 +1,23 @@
 <script>
     pagination()
 
-    document.querySelectorAll('.pagination')[0].querySelector('.prev').addEventListener("click", slow);
-    document.querySelectorAll('.pagination')[0].querySelector('.next').addEventListener("click", slow);
-    document.querySelectorAll('.pagination')[0].querySelector('ul').addEventListener("click", slow);
+    document.querySelectorAll('.pagination')[0].querySelector('.prev').addEventListener("click", showLearnMore);
+    document.querySelectorAll('.pagination')[0].querySelector('.next').addEventListener("click", showLearnMore);
+    document.querySelectorAll('.pagination')[0].querySelector('ul').addEventListener("click", showLearnMore);
 
     function pagination() {
         var buynow_emt = document.querySelectorAll('a.btn.btn-outline-secondary.btn-sm.where-to-buy.active');
-        window.setTimeout(slow, 1600);
+        window.setTimeout(showLearnMore, 1600);
         for (var i = 0; i < buynow_emt.length; i++) {
             buynow_emt[i].innerText = "Learn More"
             buynow_emt[i].href = document.querySelectorAll('p.model-name a')[i].href
         }
     }
 
-    function slow() {
+    function showLearnMore() {
         var i = 0;
         var buynow_emt = document.querySelectorAll('a.btn.btn-outline-secondary.btn-sm.where-to-buy.active');
-        window.setTimeout(slow, 1600);
+        window.setTimeout(showLearnMore, 1600);
         while (i < buynow_emt.length) {
             if (buynow_emt[buynow.length].innerText == "Learn More") {
                 break;
