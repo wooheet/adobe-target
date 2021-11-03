2697ebb602fbc48c0c26791ec3671f516b3da23d
gary <gary@spoonradio.co>
2021-11-03 11:10:53 +0900

    first commit

---
+++ at.js
@@ -0,0 +1,388 @@
+<head>
+    <style>
+        a.btn.btn-outline-secondary.btn-lg.product-enquiry {
+        -webkit-text-size-adjust: 100%;
+        -webkit-tap-highlight-color: transparent;
+        --blue: #007bff;
+        --indigo: #6610f2;
+        --purple: #6f42c1;
+        --pink: #e83e8c;
+        --red: #dc3545;
+        --orange: #fd7e14;
+        --yellow: #ffc107;
+        --green: #28a745;
+        --teal: #20c997;
+        --cyan: #17a2b8;
+        --white: #fff;
+        --gray: #6c757d;
+        --gray-dark: #343a40;
+        --primary: #007bff;
+        --secondary: #6c757d;
+        --success: #28a745;
+        --info: #17a2b8;
+        --warning: #ffc107;
+        --danger: #dc3545;
+        --light: #f8f9fa;
+        --dark: #343a40;
+        --breakpoint-xs: 0;
+        --breakpoint-sm: 576px;
+        --breakpoint-md: 768px;
+        --breakpoint-lg: 992px;
+        --breakpoint-xl: 1200px;
+        --font-family-sans-serif: -apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol","Noto Color Emoji";
+        --font-family-monospace: SFMono-Regular,Menlo,Monaco,Consolas,"Liberation Mono","Courier New",monospace;
+        word-break: normal;
+        word-wrap: normal;
+        font-family: "LG Smart","Segoe UI","Microsoft Sans Serif",sans-serif;
+        user-select: none;
+        transition: color .15s ease-in-out,background-color .15s ease-in-out;
+        text-transform: uppercase;
+        min-width: 80px;
+        font-style: normal;
+        font-weight: 700;
+        font-size: 14px;
+        line-height: 16px;
+        text-align: center;
+        white-space: normal;
+        display: inline-block;
+        vertical-align: top;
+        margin-left: 4.984424%;border: 0;
+        outline: 0;
+        padding: 13px 29px 14px;
+        text-decoration: none;
+        cursor: pointer;
+        background: 0 0!important;
+        color: #6b6b6b!important;
+        border: 0;
+        outline: 0;
+    }
+
+    </style>
+    <script>
+        document.querySelector('.pdp-info.visible-xs.mobile-bottom-info').querySelector('.wishlist-compare').appendChild(document.querySelector('a.btn.btn-outline-secondary.btn-lg.product-enquiry'))
+        document.querySelector('.relation-area').querySelector('.wishlist-compare').appendChild(document.querySelector('a.btn.btn-outline-secondary.btn-lg.product-enquiry'))
+    </script>
+</head>
+
+
+
+<head>
+    <style>
+        a.btn.btn-primary.btn-lg.add-to-cart {
+        -webkit-text-size-adjust: 100%;
+        -webkit-tap-highlight-color: transparent;
+        --blue: #007bff;
+        --indigo: #6610f2;
+        --purple: #6f42c1;
+        --pink: #e83e8c;
+        --red: #dc3545;
+        --orange: #fd7e14;
+        --yellow: #ffc107;
+        --green: #28a745;
+        --teal: #20c997;
+        --cyan: #17a2b8;
+        --white: #fff;
+        --gray: #6c757d;
+        --gray-dark: #343a40;
+        --primary: #007bff;
+        --secondary: #6c757d;
+        --success: #28a745;
+        --info: #17a2b8;
+        --warning: #ffc107;
+        --danger: #dc3545;
+        --light: #f8f9fa;
+        --dark: #343a40;
+        --breakpoint-xs: 0;
+        --breakpoint-sm: 576px;
+        --breakpoint-md: 768px;
+        --breakpoint-lg: 992px;
+        --breakpoint-xl: 1200px;
+        --font-family-sans-serif: -apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol","Noto Color Emoji";
+        --font-family-monospace: SFMono-Regular,Menlo,Monaco,Consolas,"Liberation Mono","Courier New",monospace;
+        word-break: break-word;
+        word-wrap: break-word;
+        box-sizing: border-box;
+        font-family: "LG Smart","Segoe UI","Microsoft Sans Serif",sans-serif;
+        user-select: none;
+        transition: color .15s ease-in-out,background-color .15s ease-in-out,border-color .15s ease-in-out,box-shadow .15s ease-in-out;
+        text-transform: uppercase;
+        display: inline-block;
+        min-width: 80px;
+        border-radius: 3px;
+        border: 1px solid transparent;
+        font-style: normal;
+        font-weight: 700;
+        font-size: 14px;
+        line-height: 16px;
+        text-align: center;
+        vertical-align: middle;
+        white-space: normal;
+        padding: 13px 29px 14px;
+        text-decoration: none;
+        cursor: pointer;
+        width: 100%;
+        margin-top: 10px;
+        background: 0 0!important;
+        color: #6b6b6b!important;
+        border-color: #000!important;
+    }
+
+        a.btn.btn-outline-secondary.btn-lg.where-to-buy {
+        -webkit-text-size-adjust: 100%;
+        -webkit-tap-highlight-color: transparent;
+        --blue: #007bff;
+        --indigo: #6610f2;
+        --purple: #6f42c1;
+        --pink: #e83e8c;
+        --red: #dc3545;
+        --orange: #fd7e14;
+        --yellow: #ffc107;
+        --green: #28a745;
+        --teal: #20c997;
+        --cyan: #17a2b8;
+        --white: #fff;
+        --gray: #6c757d;
+        --gray-dark: #343a40;
+        --primary: #007bff;
+        --secondary: #6c757d;
+        --success: #28a745;
+        --info: #17a2b8;
+        --warning: #ffc107;
+        --danger: #dc3545;
+        --light: #f8f9fa;
+        --dark: #343a40;
+        --breakpoint-xs: 0;
+        --breakpoint-sm: 576px;
+        --breakpoint-md: 768px;
+        --breakpoint-lg: 992px;
+        --breakpoint-xl: 1200px;
+        --font-family-sans-serif: -apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol","Noto Color Emoji";
+        --font-family-monospace: SFMono-Regular,Menlo,Monaco,Consolas,"Liberation Mono","Courier New",monospace;
+        word-break: break-word;
+        word-wrap: break-word;
+        box-sizing: border-box;
+        font-family: "LG Smart","Segoe UI","Microsoft Sans Serif",sans-serif;
+        user-select: none;
+        transition: color .15s ease-in-out,background-color .15s ease-in-out,border-color .15s ease-in-out,box-shadow .15s ease-in-out;
+        text-transform: uppercase;
+        display: inline-block;
+        min-width: 80px;
+        border-radius: 3px;
+        border: 1px solid transparent;
+        font-style: normal;
+        font-weight: 700;
+        font-size: 14px;
+        line-height: 16px;
+        text-align: center;
+        vertical-align: middle;
+        white-space: normal;
+        padding: 13px 29px 14px;
+        text-decoration: none;
+        cursor: pointer;
+        background: #a50034!important;
+        color: #fff!important;
+        border-color: #6b6b6b!important;
+        width: 100%;
+        margin-top: 10px;
+    }
+
+    </style>
+</head>
+
+
+
+
+
+<head>
+    <style>
+        a.btn.btn-primary.btn-lg.add-to-cart {
+        -webkit-text-size-adjust: 100%;
+        -webkit-tap-highlight-color: transparent;
+        --blue: #007bff;
+        --indigo: #6610f2;
+        --purple: #6f42c1;
+        --pink: #e83e8c;
+        --red: #dc3545;
+        --orange: #fd7e14;
+        --yellow: #ffc107;
+        --green: #28a745;
+        --teal: #20c997;
+        --cyan: #17a2b8;
+        --white: #fff;
+        --gray: #6c757d;
+        --gray-dark: #343a40;
+        --primary: #007bff;
+        --secondary: #6c757d;
+        --success: #28a745;
+        --info: #17a2b8;
+        --warning: #ffc107;
+        --danger: #dc3545;
+        --light: #f8f9fa;
+        --dark: #343a40;
+        --breakpoint-xs: 0;
+        --breakpoint-sm: 576px;
+        --breakpoint-md: 768px;
+        --breakpoint-lg: 992px;
+        --breakpoint-xl: 1200px;
+        --font-family-sans-serif: -apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol","Noto Color Emoji";
+        --font-family-monospace: SFMono-Regular,Menlo,Monaco,Consolas,"Liberation Mono","Courier New",monospace;
+        word-break: break-word;
+        word-wrap: break-word;
+        box-sizing: border-box;
+        font-family: "LG Smart","Segoe UI","Microsoft Sans Serif",sans-serif;
+        user-select: none;
+        transition: color .15s ease-in-out,background-color .15s ease-in-out,border-color .15s ease-in-out,box-shadow .15s ease-in-out;
+        text-transform: uppercase;
+        display: inline-block;
+        min-width: 80px;
+        border-radius: 3px;
+        border: 1px solid transparent;
+        font-style: normal;
+        font-weight: 700;
+        font-size: 14px;
+        line-height: 16px;
+        text-align: center;
+        vertical-align: middle;
+        white-space: normal;
+        padding: 13px 29px 14px;
+        text-decoration: none;
+        cursor: pointer;
+        width: 100%;
+        margin-top: 10px;
+        background: 0 0!important;
+        color: #6b6b6b!important;
+        border-color: #000!important;
+    }
+
+        a.btn.btn-outline-secondary.btn-lg.where-to-buy {
+        -webkit-text-size-adjust: 100%;
+        -webkit-tap-highlight-color: transparent;
+        --blue: #007bff;
+        --indigo: #6610f2;
+        --purple: #6f42c1;
+        --pink: #e83e8c;
+        --red: #dc3545;
+        --orange: #fd7e14;
+        --yellow: #ffc107;
+        --green: #28a745;
+        --teal: #20c997;
+        --cyan: #17a2b8;
+        --white: #fff;
+        --gray: #6c757d;
+        --gray-dark: #343a40;
+        --primary: #007bff;
+        --secondary: #6c757d;
+        --success: #28a745;
+        --info: #17a2b8;
+        --warning: #ffc107;
+        --danger: #dc3545;
+        --light: #f8f9fa;
+        --dark: #343a40;
+        --breakpoint-xs: 0;
+        --breakpoint-sm: 576px;
+        --breakpoint-md: 768px;
+        --breakpoint-lg: 992px;
+        --breakpoint-xl: 1200px;
+        --font-family-sans-serif: -apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol","Noto Color Emoji";
+        --font-family-monospace: SFMono-Regular,Menlo,Monaco,Consolas,"Liberation Mono","Courier New",monospace;
+        word-break: break-word;
+        word-wrap: break-word;
+        box-sizing: border-box;
+        font-family: "LG Smart","Segoe UI","Microsoft Sans Serif",sans-serif;
+        user-select: none;
+        transition: color .15s ease-in-out,background-color .15s ease-in-out,border-color .15s ease-in-out,box-shadow .15s ease-in-out;
+        text-transform: uppercase;
+        display: inline-block;
+        min-width: 80px;
+        border-radius: 3px;
+        border: 1px solid transparent;
+        font-style: normal;
+        font-weight: 700;
+        font-size: 14px;
+        line-height: 16px;
+        text-align: center;
+        vertical-align: middle;
+        white-space: normal;
+        padding: 13px 29px 14px;
+        text-decoration: none;
+        cursor: pointer;
+        background: #a50034!important;
+        color: #fff!important;
+        border-color: #6b6b6b!important;
+        width: 100%;
+        margin-top: 10px;
+    }
+
+        a.btn.btn-outline-secondary.btn-lg.product-enquiry {
+        -webkit-text-size-adjust: 100%;
+        -webkit-tap-highlight-color: transparent;
+        --blue: #007bff;
+        --indigo: #6610f2;
+        --purple: #6f42c1;
+        --pink: #e83e8c;
+        --red: #dc3545;
+        --orange: #fd7e14;
+        --yellow: #ffc107;
+        --green: #28a745;
+        --teal: #20c997;
+        --cyan: #17a2b8;
+        --white: #fff;
+        --gray: #6c757d;
+        --gray-dark: #343a40;
+        --primary: #007bff;
+        --secondary: #6c757d;
+        --success: #28a745;
+        --info: #17a2b8;
+        --warning: #ffc107;
+        --danger: #dc3545;
+        --light: #f8f9fa;
+        --dark: #343a40;
+        --breakpoint-xs: 0;
+        --breakpoint-sm: 576px;
+        --breakpoint-md: 768px;
+        --breakpoint-lg: 992px;
+        --breakpoint-xl: 1200px;
+        --font-family-sans-serif: -apple-system,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif,"Apple Color Emoji","Segoe UI Emoji","Segoe UI Symbol","Noto Color Emoji";
+        --font-family-monospace: SFMono-Regular,Menlo,Monaco,Consolas,"Liberation Mono","Courier New",monospace;
+        word-break: normal;
+        word-wrap: normal;
+        font-family: "LG Smart","Segoe UI","Microsoft Sans Serif",sans-serif;
+        user-select: none;
+        transition: color .15s ease-in-out,background-color .15s ease-in-out;
+        text-transform: uppercase;
+        min-width: 80px;
+        font-style: normal;
+        font-weight: 700;
+        font-size: 14px;
+        line-height: 16px;
+        text-align: center;
+        white-space: normal;
+        display: inline-block;
+        vertical-align: top;
+        margin-left: 4.984424%;border: 0;
+        outline: 0;
+        padding: 13px 29px 14px;
+        text-decoration: none;
+        cursor: pointer;
+        background: 0 0!important;
+        color: #6b6b6b!important;
+        border: 0;
+        outline: 0;
+    }
+
+    </style>
+    <script>
+        document.querySelector('.pdp-info.visible-xs.mobile-bottom-info').querySelector('.wishlist-compare').appendChild(document.querySelector('a.btn.btn-outline-secondary.btn-lg.product-enquiry'))
+        document.querySelector('.relation-area').querySelector('.wishlist-compare').appendChild(document.querySelector('a.btn.btn-outline-secondary.btn-lg.product-enquiry'))
+    </script>
+</head>
+
+
+
+var pe = document.querySelector('.relation-area').querySelector('.wishlist-compare').querySelector('a.btn.btn-outline-secondary.btn-lg.product-enquiry')
+pe.parentElement.removeChild(pe)
+var mpe = document.querySelector('.pdp-info.visible-xs.mobile-bottom-info').querySelector('.wishlist-compare').querySelector('a.btn.btn-outline-secondary.btn-lg.product-enquiry')
+mpe.parentElement.removeChild(mpe)
+
+document.querySelector('.relation-area').querySelector('.wishlist-compare').querySelector('a.link-text.ico-compare.js-compare').appendChild(document.querySelector('a.btn.btn-outline-secondary.btn-lg.product-enquiry'))
+document.querySelector('.pdp-info.visible-xs.mobile-bottom-info').querySelector('.wishlist-compare').querySelector('a.link-text.ico-compare.js-compare').appendChild(document.querySelector('a.btn.btn-outline-secondary.btn-lg.product-enquiry'))
