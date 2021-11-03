9ef3a669c6a8a139703672fcfea86e7762567eb1
gary <gary@spoonradio.co>
2021-11-03 16:52:45 +0900

    Edit 0

--- theory0.js
+++ theory0.js
@@ -1,23 +1,23 @@
 <script>
     pagination()
 
-    document.querySelectorAll('.pagination')[0].querySelector('.prev').addEventListener("click", showLearnMore);
-    document.querySelectorAll('.pagination')[0].querySelector('.next').addEventListener("click", showLearnMore);
-    document.querySelectorAll('.pagination')[0].querySelector('ul').addEventListener("click", showLearnMore);
+    document.querySelectorAll('.pagination')[0].querySelector('.prev').addEventListener("click", pagination);
+    document.querySelectorAll('.pagination')[0].querySelector('.next').addEventListener("click", pagination);
+    document.querySelectorAll('.pagination')[0].querySelector('ul').addEventListener("click", pagination);
 
-    function pagination() {
+    function showLearnMore() {
         var buynow_emt = document.querySelectorAll('a.btn.btn-outline-secondary.btn-sm.where-to-buy.active');
-        window.setTimeout(showLearnMore, 1600);
+        window.setTimeout(pagination, 1600);
         for (var i = 0; i < buynow_emt.length; i++) {
             buynow_emt[i].innerText = "Learn More"
             buynow_emt[i].href = document.querySelectorAll('p.model-name a')[i].href
         }
     }
 
-    function showLearnMore() {
+    function pagination() {
         var i = 0;
         var buynow_emt = document.querySelectorAll('a.btn.btn-outline-secondary.btn-sm.where-to-buy.active');
-        window.setTimeout(showLearnMore, 1600);
+        window.setTimeout(pagination, 1600);
         while (i < buynow_emt.length) {
             if (buynow_emt[buynow.length].innerText == "Learn More") {
                 break;
