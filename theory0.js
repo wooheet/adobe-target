96d24eddf5d89739d430c29a64829c15bb4c83f0
tedy <30405705+wooheet@users.noreply.github.com>
2021-11-04 17:05:13 +0900

    Update theory0.js

--- theory0.js
+++ theory0.js
@@ -16,7 +16,7 @@
     function pagination() {
         var i = 0;
         var wtb_emt = document.querySelectorAll('a.btn.btn-outline-secondary.btn-sm.where-to-buy.active');
-        window.setTimeout(pagination, 500);
+        window.setTimeout(pagination, 100);
         while (i < wtb_emt.length) {
             wtb_emt[i].innerText = "learn more"
             wtb_emt[i].href = document.querySelectorAll('p.model-name a')[i].href
