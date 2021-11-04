185dd2962d591851ba354f5cedf66f4feda77a3f
tedy <30405705+wooheet@users.noreply.github.com>
2021-11-04 17:01:11 +0900

    Update theory0.js

--- theory0.js
+++ theory0.js
@@ -1,9 +1,9 @@
 <script>
     showLearnMore()
 
-    document.querySelectorAll('.pagination')[0].querySelector('.prev').addEventListener("click", showLearnMore);
-    document.querySelectorAll('.pagination')[0].querySelector('.next').addEventListener("click", showLearnMore);
-    document.querySelectorAll('.pagination')[0].querySelector('ul').addEventListener("click", showLearnMore);
+    document.querySelectorAll('.pagination')[0].querySelector('.prev').addEventListener("click", pagination);
+    document.querySelectorAll('.pagination')[0].querySelector('.next').addEventListener("click", pagination);
+    document.querySelectorAll('.pagination')[0].querySelector('ul').addEventListener("click", pagination);
 
     function showLearnMore() {
         var wtb_emt = document.querySelectorAll('a.btn.btn-outline-secondary.btn-sm.where-to-buy.active');
@@ -13,6 +13,16 @@
         }
     }
 
+    function pagination() {
+        var i = 0;
+        var wtb_emt = document.querySelectorAll('a.btn.btn-outline-secondary.btn-sm.where-to-buy.active');
+        window.setTimeout(pagination, 500);
+        while (i < wtb_emt.length) {
+            wtb_emt[i].innerText = "learn more"
+            wtb_emt[i].href = document.querySelectorAll('p.model-name a')[i].href
+            i++;
+        }
+    }
 
     (function($) {
         $(document).ajaxSuccess(function( event, xhr, settings ) {
@@ -23,6 +33,7 @@
                 'eventAction': settings.url + (settings.type == 'POST' && settings.data ? ';' + settings.data : ''),
                 'eventLabel': xhr.responseText
             });
+
         });
     })(jQuery);
 </script>
